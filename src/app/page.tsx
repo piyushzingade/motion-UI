ee768338aa4c244ea9e3f6fef09cb29ff19ed606
piyushzingade <piyushzingade@gmail.com>
2026-01-03 23:45:10 +0530

    updated hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,16 @@ import Component6 from "@/components/component-6";
 import Component7 from "@/components/component-7";
 import Component7Light from "@/components/component-7-light";
 import Component8 from "@/components/component-8";
+import Hero1 from "@/components/hero-1";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className=" min-h-screen">
       {/* Decorative background elements to show off glass blur */}
       {/* <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-purple-200/40 rounded-full blur-[120px] -z-10" />
       <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-blue-200/40 rounded-full blur-[120px] -z-10" /> */}
 
-      <Component8 />
+      <Hero1 />
     </div>
   );
 }
